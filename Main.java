6a87989ac94363fdb9dca7050fd62a033f797469
Donna Svinis <donnasvinis@zipcoders-MBP-3.fios-router.home>
2019-05-22 18:59:39 -0400

    completed problem

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Created by Donna Svinis on 05/22/2019
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
 
+        Scanner in = new Scanner(System.in);
+        int n;
+        int total = 0;
+        
+        System.out.println("Input a whole number.");
+        n = in.nextInt(); 
+        for (int i = 1; i <= n; i++) {
+            total = total + i;
+        }
+        System.out.println("The sum of the numbers 1 to " + n + " is: " + total);
     }
 }
